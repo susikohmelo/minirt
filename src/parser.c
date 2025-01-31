f481e3b57ef49ccf0151196c9d1f661a43fa9bf8
Lauri Fiestas <lfiestas@c1r6p19.hive.fi>
2025-01-31 17:58:23 +0200

    Added float parsing

--- src/parser.c
+++ src/parser.c
@@ -6,7 +6,7 @@
 /*   By: lfiestas <lfiestas@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 16:03:43 by lfiestas          #+#    #+#             */
-/*   Updated: 2025/01/31 17:23:58 by lfiestas         ###   ########.fr       */
+/*   Updated: 2025/01/31 17:57:57 by lfiestas         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,18 +34,40 @@ double	str_to_f(const char *str)
 	integer = 0;
 	while ('0' <= *str && *str <= '9' && integer < UINT64_MAX / 10 - 10)
 		integer = integer * 10 + *str++ - '0';
+	if (integer >= UINT64_MAX / 10 - 10)
+		return (sign * INFINITY);
 	if (*str == '.')
 		++str;
 	fraction = 0;
 	fraction_power = 1;
-	while ('0' <= *str && *str <= '9' && fraction < UINT64_MAX / 10 - 10)
+	while ('0' <= *str && *str <= '9' && fraction_power < 1000000000)
 	{
 		fraction_power *= 10;
 		fraction = fraction * 10 + *str++ - '0';
 	}
-	if (integer >= UINT64_MAX / 10 - 10 || fraction >= UINT64_MAX / 10 - 10)
-		return (sign * INFINITY);
 	return (sign * (integer + (double)fraction / fraction_power));
+}
+
+const char	*parse_float(t_minirt *mrt, double *f, const char *line, char sep)
+{
+	bool	digit_found;
+
+	digit_found = false;
+	*f = str_to_f(line);
+	if (*line == '+' || *line == '-')
+		++line;
+	while (ft_isdigit(*line))
+		digit_found = ++line;
+	if (*line == '.')
+		++line;
+	while (ft_isdigit(*line))
+		digit_found = ++line;
+	mrt_assert(mrt, digit_found, "Invalid numerical value");
+	if (sep == ' ')
+		mrt_assert(mrt, ft_isspace(*line), "Invalid numerical value");
+	else
+		mrt_assert(mrt, *line == sep, "Invalid numerical value");
+	return (line + sizeof sep);
 }
 
 static bool	parse_ambient_light(t_minirt *mrt, const char *line)
@@ -62,19 +84,19 @@ static void	parse_line(t_minirt *mrt, bool found[128], const char *line)
 	if (line[0] == 'A' && ft_isspace(line[1])
 		&& mrt_assert(mrt, !found['A'], "Multiple ambient lights"))
 		found['A'] = parse_ambient_light(mrt, line + ft_strlen("A "));
-	else if (line[0] == 'C' && ft_isspace(line[1])
-		&& mrt_assert(mrt, !found['C'], "Multiple cameras"))
-		found['C'] = parse_camera(mrt, line + ft_strlen("C "));
-	else if (line[0] == 'L' && ft_isspace(line[1])
-		&& mrt_assert(mrt, !found['L'], "Multiple lights"))
-		found['L'] = parse_ligth(mrt, line + ft_strlen("L "));
-	else if (line[0] == 's' && line[1] == 'p' && ft_isspace(line[2]))
-		parse_sphere(mrt, line + ft_strlen("sp "));
-	else if (line[0] == 'p' && line[1] == 'l' && ft_isspace(line[2]))
-		parse_plane(mrt, line + ft_strlen("pl "));
-	else if (line[0] == 'c' && line[1] == 'y' && ft_isspace(line[2]))
-		parse_cylinder(mrt, line + ft_strlen("cy "));
-	mrt_assert(mrt, false, "Invalid line in input file");
+	// else if (line[0] == 'C' && ft_isspace(line[1])
+	// 	&& mrt_assert(mrt, !found['C'], "Multiple cameras"))
+	// 	found['C'] = parse_camera(mrt, line + ft_strlen("C "));
+	// else if (line[0] == 'L' && ft_isspace(line[1])
+	// 	&& mrt_assert(mrt, !found['L'], "Multiple lights"))
+	// 	found['L'] = parse_ligth(mrt, line + ft_strlen("L "));
+	// else if (line[0] == 's' && line[1] == 'p' && ft_isspace(line[2]))
+	// 	parse_sphere(mrt, line + ft_strlen("sp "));
+	// else if (line[0] == 'p' && line[1] == 'l' && ft_isspace(line[2]))
+	// 	parse_plane(mrt, line + ft_strlen("pl "));
+	// else if (line[0] == 'c' && line[1] == 'y' && ft_isspace(line[2]))
+	// 	parse_cylinder(mrt, line + ft_strlen("cy "));
+	mrt_assert(mrt, false, "Unknown element type");
 }
 
 void	parse_input(t_minirt *mrt, const char *path)
@@ -87,10 +109,10 @@ void	parse_input(t_minirt *mrt, const char *path)
 	while (true)
 	{
 		mrt->line = get_next_line(fd);
-		if (line == NULL)
+		if (mrt->line == NULL)
 			break;
 		while (ft_isspace(mrt->line[ft_strlen(mrt->line) - 1]))
-			mrt->line[ft_strlen(line) - 1] = '\0';
+			mrt->line[ft_strlen(mrt->line) - 1] = '\0';
 		if (mrt->line[0] != '\0')
 			parse_line(mrt, found, mrt->line);
 		free(mrt->line);
