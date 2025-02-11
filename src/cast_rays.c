8a499308a62560acfa71edadcc6425ba3b20376d
Lassi Jylhankangas <ljylhank@c1r5p21.hive.fi>
2025-02-11 17:39:23 +0200

    changed reflection strength equation

--- src/cast_rays.c
+++ src/cast_rays.c
@@ -6,7 +6,7 @@
 /*   By: ljylhank <ljylhank@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/31 15:21:40 by ljylhank          #+#    #+#             */
-/*   Updated: 2025/02/11 17:35:37 by ljylhank         ###   ########.fr       */
+/*   Updated: 2025/02/11 17:38:06 by ljylhank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -213,7 +213,7 @@ t_vec3	surface_color(t_minirt *m, t_ray data, bool is_reflection)
 	data.is_reflect = 0.5;
 	if (data.shape->roughness_map)
 		data.is_reflect = reflect;
-	reflect = pow(1 - reflect / (0.5 + reflect) * 1.5, 2);
+	reflect = pow(1 - reflect / (0.65 + reflect) * 1.65, 2);
 	cmr_dir = vec3_normalize(vec3_sub(m->camera_coords, ray));
 	data.dir = vec3_sub(vec3_muls(normal, 2 * vec3_dot(cmr_dir, normal)), cmr_dir);
 	data.start = vec3_add(ray, vec3_muls(data.dir, 0.001));
