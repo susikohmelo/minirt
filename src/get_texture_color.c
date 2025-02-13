7d0ed6fb448b09e7f40a8573efc9e8b8d53ce9d5
Lassi Jylhankangas <ljylhank@c1r6p17.hive.fi>
2025-02-13 15:41:36 +0200

    adjusted reflection blur

--- src/get_texture_color.c
+++ src/get_texture_color.c
@@ -6,7 +6,7 @@
 /*   By: ljylhank <ljylhank@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/12 21:11:33 by ljylhank          #+#    #+#             */
-/*   Updated: 2025/02/12 21:39:09 by ljylhank         ###   ########.fr       */
+/*   Updated: 2025/02/13 15:41:25 by ljylhank         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ t_vec3	get_albedo_blur(t_vec3 intersect, const t_shape *shape,
 	if (uv.x > 1. || uv.x < 0. || uv.y > 1. || uv.y < 0.)
 		return (vec3(0, 0, 0));
 	img = shape->texture;
-	vect = get_texture_from_uv(img, uv.x, uv.y, blur / (0.2 + blur) * 1.2);
+	vect = get_texture_from_uv(img, uv.x, uv.y, blur / (0.4 + blur) * 1.4);
 	return (vect);
 }
 
