5e89636bd11becc0c68cfa49ab5d70ed64b7a0be
Lorenzo Fiestas <hik.lorenzo@hotmail.com>
2025-02-16 13:38:05 +0200

    Added global attributes editing

--- src/edit_objects.c
+++ src/edit_objects.c
@@ -74,6 +74,23 @@ static void	edit_light(t_minirt *m, t_light *light, double x)
 	light->color = vec3_muls(light->color_value, light->brightness);
 }
 
+static void	edit_attributes(t_minirt *m, double x)
+{
+	if (m->moving_slider == 1)
+		m->ambient_light_ratio = x;
+	if (m->moving_slider == 2)
+		m->ambient_light_color.r = x;
+	if (m->moving_slider == 3)
+		m->ambient_light_color.g = x;
+	if (m->moving_slider == 4)
+		m->ambient_light_color.b = x;
+	if (m->moving_slider == 5)
+		m->camera_field_of_view = 180 * x;
+	if (1 <= m->moving_slider && m->moving_slider <= 4)
+		m->ambient_light = vec3_muls( \
+			m->ambient_light_color, m->ambient_light_ratio);
+}
+
 void	edit_objects(t_minirt *m, double x)
 {
 	x = fmin(fmax(x / (LINE_LENGTH * CHAR_WIDTH), 0), 1);
@@ -85,5 +102,7 @@ void	edit_objects(t_minirt *m, double x)
 		edit_cylinder(m, m->shape, x);
 	if (m->shape_type == SHAPE_LIGHT)
 		edit_light(m, (t_light *)m->shape, x);
+	if (m->shape_type == SHAPE_GLOBAL_ATTRIBUTES)
+		edit_attributes(m, x);
 	redraw(m);
 }
